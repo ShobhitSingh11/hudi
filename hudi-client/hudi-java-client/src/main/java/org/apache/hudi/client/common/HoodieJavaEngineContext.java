dcda6c4609f39ab4e77406c2ea2c3a02f770099e
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-01-25 02:38:08 +0530

    Update HoodieJavaEngineContext.java

--- hudi-client/hudi-java-client/src/main/java/org/apache/hudi/client/common/HoodieJavaEngineContext.java
+++ hudi-client/hudi-java-client/src/main/java/org/apache/hudi/client/common/HoodieJavaEngineContext.java
@@ -79,6 +79,7 @@ public class HoodieJavaEngineContext extends HoodieEngineContext {
 
   @Override
   public <T> HoodieData<T> parallelize(List<T> data, int parallelism) {
+    // kjldwe
     return HoodieListData.eager(data);
   }
 
